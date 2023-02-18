37fee288c68a239df5eac97c4aee56e3bc670c33
Juan Pablo Olivadoti <jpo.workmail@gmail.com>
2023-02-18 22:49:23 +0100

    ANGULAR DIST DIRECTORY PATH CAPACITOR DIST DIRECTORY PATH

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.argentinosporespana.app',
   appName: 'AxE App',
-  webDir: 'dist',
+  webDir: 'dist/angularCapacitor',
   bundledWebRuntime: false
 };
 
