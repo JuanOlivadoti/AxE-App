15d57c6000da89fef11062ccd0dc030f0a33a88b
Juan Pablo Olivadoti <jpo.workmail@gmail.com>
2023-02-19 22:54:40 +0100

    ANDROID bundle IOS bundle
    fix angular dist path
    fix capacitor dist path

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.argentinosporespana.app',
   appName: 'AxE-App',
-  webDir: 'dist',
+  webDir: 'dist/AxE-app',
   bundledWebRuntime: false
 };
 
